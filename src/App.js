35b4787c63ee28ecab5bb15e4c8b8ec3ecde98e2
Dhruv Sharma <dhruvsharma@Dhruvs-MacBook-Pro-2.local>
2025-01-29 08:56:48 +0530

    Resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Hello, React!</h1>
+            <p>This is my first React app running in VS Code.</p>
+        </div>
+    );
+}
+
+export default App;
